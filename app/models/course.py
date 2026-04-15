4b7f5b44ca0c4af58594a16de84d0ee0a4f96d64
austenyt <austenyt@gmail.com>
2026-04-15 23:32:57 +0300

    ORM added

--- app/models/course.py
+++ app/models/course.py
@@ -1,5 +1,12 @@
 from app.db.base import Base
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import String
+from sqlalchemy import Integer
 
 
 class Course(Base):
     __tablename__ = "course"
+
+    name: Mapped[str] = mapped_column(String(150))
+    qty: Mapped[int] = mapped_column(Integer)

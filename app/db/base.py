b52e616e370eebc38e22d030a95425a6bf0c4fff
austenyt <austenyt@gmail.com>
2026-04-20 23:55:36 +0300

    comments

--- app/db/base.py
+++ app/db/base.py
@@ -1,3 +1,5 @@
 from sqlalchemy.orm import declarative_base
-
+"""
+Создаем базовый класс, который будет служить основой для всех ORM-моделей
+"""
 Base = declarative_base()
--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+"""
+Адрес для подключения к БД:
+"""
 DATABASE_URL = 'sqlite:///./lms.db'
 
+"""
+Создание движка для управления подключениями к БД:
+Передаем адрес для подключения и дополнительные параметры - отключение проверки использования единого потока.
+"""
 engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
+"""
+Создание локальной сессии подключения с помощью класса sessionmaker.
+Передаем параметры отключение автоматического сохранения сессии, отключение автоматического сброса, привязывание сессии к движку.
+"""
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

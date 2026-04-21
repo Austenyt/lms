6810df930d7d7eb06191d4dbec500f288a50bee1
Marat Musin <134961170+Austenyt@users.noreply.github.com>
2026-04-21 09:29:02 +0300

    Update course.py

--- app/models/course.py
+++ app/models/course.py
@@ -4,10 +4,17 @@ from sqlalchemy.orm import mapped_column
 from sqlalchemy import String
 from sqlalchemy import Integer
 
-
+"""
+Создание класса для создания таблицы
+"""
 class Course(Base):
+    """
+    Создание таблицы в БД
+    """
     __tablename__ = "course"
-
+"""
+Задание полей таблицы с описанием типов и атрибутов
+"""
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(150))
     qty: Mapped[int] = mapped_column(Integer)
